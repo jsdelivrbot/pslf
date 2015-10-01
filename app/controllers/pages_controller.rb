9c1a8e7948deedfc4284e3aa968c37fd5a1db49c
gmbiles <grant@gradible.com>
2015-10-01 13:09:40 -0400

    added pages and created header

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def overview
+  end
+
+  def eligibility
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "overview" => "pages#overview"
+  get "eligibility" => "pages#eligibility"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
