e03e83ab63b15d3bf260562dfeeed8850800a80e
gmbiles <grant@gradible.com>
2016-01-22 16:39:30 -0500

    fixed download errors

--- app/models/cert.rb
+++ app/models/cert.rb
@@ -4,6 +4,6 @@ class Cert < ActiveRecord::Base
 	#has_attached_file :image
 	#validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif","image/pdf"]
 
-	has_attached_file :document
+  has_attached_file :document
 	validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 end
